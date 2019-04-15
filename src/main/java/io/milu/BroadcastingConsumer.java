791ef4b1fb0d39dcf583a08b167da2e7664806d7
MiluDeer <271905946@qq.com>
2019-04-15 22:27:25 +0800

    MARK

---
+++ src/main/java/io/milu/BroadcastingConsumer.java
@@ -0,0 +1,41 @@
+package io.milu;
+
+import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
+import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
+import org.apache.rocketmq.common.consumer.ConsumeFromWhere;
+import org.apache.rocketmq.common.message.MessageExt;
+import org.apache.rocketmq.common.protocol.heartbeat.MessageModel;
+
+import java.util.List;
+
+public class BroadcastingConsumer {
+    public static void main(String [] argv) throws Exception {
+        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("consumer_broadcasting");
+        consumer.setInstanceName(argv[0]);
+        consumer.setNamesrvAddr("127.0.0.1:9876");
+        consumer.setConsumeFromWhere(ConsumeFromWhere.CONSUME_FROM_FIRST_OFFSET);
+        consumer.subscribe("TopicTest", "*");
+
+        consumer.setMessageModel(MessageModel.BROADCASTING);
+
+        //设置一个Listener，主要进行消息的逻辑处理
+        consumer.registerMessageListener(new MessageListenerConcurrently() {
+            @Override
+            public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> msgs, ConsumeConcurrentlyContext context) {
+                System.out.println(argv[0] + "收到消息");
+
+                //返回消费状态
+                //CONSUME_SUCCESS 消费成功
+                //RECONSUME_LATER 消费失败，需要稍后重新消费
+                return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+            }
+        });
+
+        //调用start()方法启动consumer
+        consumer.start();
+
+        System.out.println("Consumer Started.");
+    }
+}
---
+++ src/main/java/io/milu/ClusteringConsumer.java
@@ -0,0 +1,37 @@
+package io.milu;
+
+import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
+import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
+import org.apache.rocketmq.common.consumer.ConsumeFromWhere;
+import org.apache.rocketmq.common.message.MessageExt;
+
+import java.util.List;
+
+public class ClusteringConsumer {
+
+    public static void main(String [] argv) throws Exception {
+        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("consumer1");
+        consumer.setInstanceName(argv[0]);
+        consumer.setNamesrvAddr("127.0.0.1:9876");
+        consumer.setConsumeFromWhere(ConsumeFromWhere.CONSUME_FROM_FIRST_OFFSET);
+        consumer.subscribe("TopicTest", "*");
+
+        consumer.registerMessageListener(new MessageListenerConcurrently() {
+            @Override
+            public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> msgs, ConsumeConcurrentlyContext context) {
+                System.out.println(argv[0] + "收到消息");
+
+                //返回消费状态
+                //CONSUME_SUCCESS 消费成功
+                //RECONSUME_LATER 消费失败，需要稍后重新消费
+                return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+            }
+        });
+
+        consumer.start();
+
+        System.out.println("Consumer Started.");
+    }
+}
